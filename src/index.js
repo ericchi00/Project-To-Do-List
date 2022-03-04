c61ead859b891303c2337c8a8633b0f4869b1bd3
ericchi00 <ericchi00@gmail.com>
2022-03-04 13:00:18 -0800

    added function to add obj to array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './style.css';
+
+const toDoObj = (title, description, dueDate, priority) => {
+    return { title, description, dueDate, priority };
+};
+
+const projectList = (() => {
+    const listArray = [];
+    const submit = document.querySelector('#submit');
+    const form = document.querySelector('')
+
+    function addToList() {
+        submit.addEventListener('click', () => {
+            const itemObj = new toDoObj();
+            addObjToArray(itemObj);
+
+        })
+    }
+    function addObjToArray(obj) {
+        obj.title = document.querySelector('#title').value;
+        obj.description = document.querySelector('#description').value;
+        obj.dueDate = document.querySelector('#dueDate').value;
+        obj.priority = document.querySelector('#priority');
+        projectList.listArray.push(obj);
+    }
+
+    return {listArray}
+})();
+
+
+
+// creates new projects for user to select from
+const projects = (() => {
+    const newArray = [];
+
+
+})()
+
+
+export {projectList, toDoObj}
