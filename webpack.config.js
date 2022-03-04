7508b7bdba2cffc7cf0aeb32e43bcf42c38f9d64
ericchi00 <ericchi00@gmail.com>
2022-03-04 13:00:59 -0800

    added images & font/img folder path

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+                generator: {
+                    filename: 'css/[hash][ext][query]'
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[hash][ext][query]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font/[hash][ext][query]'
+                }
+            },
+        ],
+    },
+};
