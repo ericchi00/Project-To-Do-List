882907fcc967da5c8006db4fe6e1d52ddd6b2ecc
ericchi00 <ericchi00@gmail.com>
2022-03-04 16:09:47 -0800

    added ability to add items to array and display

--- src/index.js
+++ src/index.js
@@ -1,29 +1,61 @@
 import './style.css';
+import { createsToDoItem } from './dom.js';
+import { format } from 'date-fns'
+import { parseISO } from 'date-fns'
 
-const toDoObj = (title, description, dueDate, priority) => {
-    return { title, description, dueDate, priority };
+
+
+const Item = (title, dueDate, priority, description ) => {
+    return { title, dueDate, priority, description };
 };
 
 const projectList = (() => {
     const listArray = [];
     const submit = document.querySelector('#submit');
-    const form = document.querySelector('')
+    const form = document.querySelector('#form')
 
+    function displayItems(arr) {
+        createsToDoItem.createCard();
+        const cards = document.querySelectorAll('.card');
+        arr.forEach((item, index) => {
+            document.querySelectorAll('.title')[index].textContent = 'Title: ' + `${item.title}`;
+            document.querySelectorAll('.dueDate')[index].textContent = 'Due Date: ' + `${item.dueDate}`;
+            document.querySelectorAll('.priority')[index].textContent = 'Priority: ' + `${item.priority}`;
+            document.querySelectorAll('.description')[index].textContent = `${item.description}`;
+            cards[index].setAttribute('cardCount', index);
+        });
+    }
+
+    addToList();
     function addToList() {
         submit.addEventListener('click', () => {
-            const itemObj = new toDoObj();
-            addObjToArray(itemObj);
-
+            const itemObj = Item(
+                document.querySelector('#title').value,
+                format(new Date(parseISO(document.querySelector('#dueDate').value)), 'MM/dd/yyyy hh:mm'),
+                document.querySelector('input[name=priority]:checked').value,
+                document.querySelector('#description').value,
+            );
+            listArray.push(itemObj);
+            console.table(listArray);
+            displayItems(listArray);
+            form.reset();
+            closeForm();
         })
     }
-    function addObjToArray(obj) {
-        obj.title = document.querySelector('#title').value;
-        obj.description = document.querySelector('#description').value;
-        obj.dueDate = document.querySelector('#dueDate').value;
-        obj.priority = document.querySelector('#priority');
-        projectList.listArray.push(obj);
+
+    closeOpenForm();
+    function closeOpenForm() {
+        document.querySelector('#newItem').addEventListener('click', openForm);
+        document.querySelector('#close').addEventListener('click', closeForm);
     }
 
+    function openForm() {
+        document.querySelector('.formWrapper').style.display = 'flex';
+    }
+
+    function closeForm() {
+        document.querySelector('.formWrapper').style.display = 'none';
+    }
     return {listArray}
 })();
 
@@ -36,5 +68,3 @@ const projects = (() => {
 
 })()
 
-
-export {projectList, toDoObj}
